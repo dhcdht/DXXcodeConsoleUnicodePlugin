a2262764962c0212b87dd96f814f7ec0bdd97170
dhcdht <dhc@zhihu.com>
2014-07-02 10:01:18 +0800

    Initial Commit

---
+++ XcodeConsoleUnicodePlugin/DXXcodeConsoleUnicodePlugin.h
@@ -0,0 +1,13 @@
+//
+//  DXXcodeConsoleUnicodePlugin.h
+//  DXXcodeConsoleUnicodePlugin
+//
+//  Created by dhcdht on 14-7-2.
+//  Copyright (c) 2014年 dhcdht. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface DXXcodeConsoleUnicodePlugin : NSObject
+
+@end
